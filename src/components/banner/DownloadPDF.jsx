ceafdecf0a5c17ec26073551993408be5d4a7767
Dineshpalappdeveloper <dineshkr748199@gmail.com>
2024-06-23 06:48:21 +0530

    pdf test

--- src/components/banner/DownloadPDF.jsx
+++ src/components/banner/DownloadPDF.jsx
@@ -1,26 +1,53 @@
 import React from "react";
+import axios from "axios";
+import { saveAs } from "file-saver";
 // import { FaDownload } from "react-icons/fa";
 
 const DownloadPDF = () => {
-  const handleDownload = () => {
+  const handleDownload = async () => {
     const pdfUrl = process.env.PUBLIC_URL + "/resume.pdf"; // Path to your PDF file
 
-    // Use a library or create an anchor element to initiate the download
-    const anchor = document.createElement("a");
-    anchor.href = pdfUrl;
-    anchor.download = "resume.pdf";
-    anchor.click();
+    try {
+      const response = await axios({
+        url: pdfUrl,
+        method: "GET",
+        responseType: "blob", // Important
+      });
+
+      // Using file-saver library to save the PDF file
+      saveAs(response.data, "resume.pdf");
+    } catch (error) {
+      console.error("Error fetching or saving PDF: ", error);
+      // Handle error, e.g., show an error message to the user
+    }
+  };
+  const onButtonClick = () => {
+    // using Java Script method to get PDF file
+    fetch("SamplePDF.pdf").then((response) => {
+      response.blob().then((blob) => {
+        // Creating new object of PDF file
+        const fileURL = window.URL.createObjectURL(blob);
+
+        // Setting various property values
+        let alink = document.createElement("a");
+        alink.href = fileURL;
+        alink.download = "SamplePDF.pdf";
+        alink.click();
+      });
+    });
   };
 
   return (
     <div>
       <button
-        className="bg-black-800 border  border-red-500 hover:bg-black-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+        className="bg-black-800 border border-red-500 hover:bg-black-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
         onClick={handleDownload}
       >
         {/* <FaDownload className="inline-block mr-2" /> */}
         Download CV
       </button>
+
+      <button onClick={onButtonClick}>Download PDF</button>
     </div>
   );
 };
